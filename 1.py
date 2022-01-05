42884d8505b00f2b1d6675768be1220b9a8138a1
Joshua Equere <marveloper.8@gmail.com>
2022-01-05 16:56:57 +0100

    python for loop

--- 1.py
+++ 1.py
@@ -369,25 +369,76 @@
 # if b > a:
 #     pass
 
-i = 1
-while i < 6:
-    print(i)
-    i += 1
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
 
-while i < 6:
-    print(i)
-    if i == 3:
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# while i < 6:
+#     print(i)
+#     i += 1
+# else:
+#     print("i is no longer less than 6")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+for x in "banana":
+    print(x)
+
+for x in fruits:
+    print(x)
+    if x == "banana":
         break
-    i += 1
 
-while i < 6:
-    i += 1
-    if i == 3:
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
+for x in fruits:
+    if x == "banana":
         continue
-    print(i)
+    print(x)
 
-while i < 6:
-    print(i)
-    i += 1
+for x in range(6):
+    print(x)
+
+for x in range(2, 6):
+    print(x)
+
+for x in range(2, 30, 3):
+    print(x)
+
+for x in range(6):
+    print(x)
 else:
-    print("i is no longer less than 6")
+    print("Finally finished!")
+
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print("Finally finished!")
+
+adj = ["red", "big", "tasty"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+for x in [0, 1, 2]:
+    pass
