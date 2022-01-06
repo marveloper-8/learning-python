ec15910b972a4fde484aa91e1ad6254d2e0d4010
Joshua Equere <marveloper.8@gmail.com>
2022-01-06 20:38:02 +0100

    math

--- 2.py
+++ 2.py
@@ -1,14 +1,78 @@
-import mymodule as mx
-import platform
+# import mymodule as mx
+# import platform
 
-a = mx.person1["age"]
+# a = mx.person1["age"]
+# print(a)
+
+# x = platform.system()
+# print(x)
+
+# x1 = dir(platform)
+# print(x1)
+
+# from mymodule import person1
+# print(person1["age"])
+
+# import datetime
+
+# a = datetime.datetime.now()
+# print(a)
+# print(a.year)
+# print(a.strftime("%A"))
+
+# b = datetime.datetime(2020, 5, 17)
+# print(b)
+
+# c = datetime.date(2018, 6, 1)
+# print(c.strftime("%B"))
+
+# print(a.strftime("%a"))
+# print(a.strftime("%A"))
+# print(a.strftime("%w"))
+# print(a.strftime("%d"))
+# print(a.strftime("%b"))
+# print(a.strftime("%B"))
+# print(a.strftime("%m"))
+# print(a.strftime("%y"))
+# print(a.strftime("%Y"))
+# print(a.strftime("%H"))
+# print(a.strftime("%I"))
+# print(a.strftime("%p"))
+# print(a.strftime("%M"))
+# print(a.strftime("%S"))
+# print(a.strftime("%f"))
+# print(a.strftime("%j"))
+# print(a.strftime("%U"))
+# print(a.strftime("%W"))
+# print(a.strftime("%c"))
+# print(a.strftime("%C"))
+# print(a.strftime("%x"))
+# print(a.strftime("%X"))
+# print(a.strftime("%%"))
+# print(a.strftime("%G"))
+# print(a.strftime("%u"))
+# print(a.strftime("%V"))
+
+a = min(5, 10, 25)
+b = max(5, 10, 25)
 print(a)
+print(b)
 
-x = platform.system()
-print(x)
+c = abs(-7.25)
+print(c)
 
-x1 = dir(platform)
-print(x1)
+d = pow(4, 3)
+print(d)
 
-from mymodule import person1
-print(person1["age"])
+import math
+
+e = math.sqrt(64)
+print(e)
+
+f = math.ceil(1.4)
+g = math.floor(1.4)
+print(f)
+print(g)
+
+h = math.pi
+print(h)
