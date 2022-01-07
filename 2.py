558871871da9e39fb70522c809001fe730fdbd54
Joshua Equere <marveloper.8@gmail.com>
2022-01-07 17:00:18 +0100

    string formatting

--- 2.py
+++ 2.py
@@ -391,5 +391,29 @@
 # if not type(b) is int:
 #     raise TypeError("Only integers are allowed")
 
-username = input("Enter username:")
-print("Username is: " + username)
+# username = input("Enter username:")
+# print("Username is: " + username)
+
+price = 49
+a = "The price is {} dollars"
+print(a.format(price))
+
+b = "The price is {:.2f} dollars"
+print(b.format(price))
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+myorder2 = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder2.format(quantity, itemno, price))
+
+age = 36
+name = "John"
+c = "His name is {1}. {1} is {0} years old."
+print(c.format(age, name))
+
+d = "I have a {carname}, it is a {model}."
+print(d.format(carname = "Ford", model = "Mustang"))
