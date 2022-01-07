83fccad641ce555b74a11af322ca6698a8224f6e
Joshua Equere <marveloper.8@gmail.com>
2022-01-07 19:54:32 +0100

    file write

--- 3.py
+++ 3.py
@@ -1,10 +1,26 @@
-f = open("demofile.txt", "r")
+# f = open("demofile.txt", "r")
 # print(f.read())
 # print(f.read(5))
-print(f.readline())
+# print(f.readline())
 # print(f.readline())
 
 # for x in f:
 #     print(x)
 
-f.close()
+# f.close()
+
+# f = open("demofile2.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+# f = open("demofile2.txt", "r")
+# print(f.read())
+
+# f = open("demofile3.txt", "w")
+# f.write("Woops! I have new content!")
+# f.close()
+
+# f = open("demofile3.txt", "r")
+# print(f.read())
+
+f = open("myfile.txt", "x")
